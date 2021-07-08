857db3e67540b2654437a5293a62077029ef46c6
KAZU-kaa <72007272+KAZU-kaa@users.noreply.github.com>
2021-07-08 11:43:56 +0900

    受け取り側

--- main.ts
+++ main.ts
@@ -1,29 +1,18 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    music.playMelody("- - - - - - - - ", 120)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . # . # .
+        # . # . .
+        . # . . .
+        `)
+})
 radio.setGroup(1)
-radio.setTransmitPower(4)
-let 電流 = pins.analogReadPin(AnalogPin.P0)
 basic.showLeds(`
     . # # # #
-    . # . . #
-    . # . . #
+    . # # # #
+    . # # # #
     . # # # #
     . . . . .
     `)
-basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.P0) > 電流 + 300) {
-        radio.sendNumber(0)
-        basic.showLeds(`
-            # . # . #
-            . # . # .
-            # . # . #
-            . # . # .
-            # . # . #
-            `)
-        basic.showLeds(`
-            . # . # .
-            # . # . #
-            . # . # .
-            # . # . #
-            . # . # .
-            `)
-    }
-})
